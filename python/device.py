b5e3030a79030862679a556093e86d05a5fa4cbc
gtechoogle <gtechoogle@gmail.com>
2018-04-18 00:23:10 +0800

    update

--- python/device.py
+++ python/device.py
@@ -2,26 +2,32 @@ import subprocess
 
 deviceInfo=[]
 def connectDevcie():
-    try:
-        output= subprocess.check_output('adb devices -l').split("\r\n")
-        for item in output:
-            if item != '':
-                deviceInfo.append(item)
-        if len(deviceInfo) <= 1:
-            return False
-        else:
-            return True
-    except Exception,e:  
-        print "Device Connect Fail:",e
+    # try:
+    output = subprocess.check_output('adb devices -l').split("\r\n".encode(encoding="utf-8"))
+    for item in output:
+        item = bytes.decode(item)
+        if item != '':
+            deviceInfo.append(item)
+            print('-----')
+            print(item)
+    if len(deviceInfo) <= 1:
+        return False
+    else:
+        return True
+    # except Exception, e:  
+    #     print "Device Connect Fail:",e
 
 def main():
     connected = connectDevcie()
     if connected:
-        print "Device connected!"
+        print ("Device connected!")
         if len(deviceInfo) > 2:
-            print "More than one device connected, please check it!"
+            print ("More than one device connected, please check it!")
+        else:
+            cmd='Python E:\\Open_Source_Code\\Github\\performance_tool\\tools\\systrace\\systrace.py -b 32768 -t 5 -o mytrace.html wm gfx input view sched freq'
+            output = subprocess.Popen(cmd)
     else:
-        print "Please make sure the device is connected!"
+        print ("Please make sure the device is connected!")
 
 if __name__ == '__main__':
     main()
